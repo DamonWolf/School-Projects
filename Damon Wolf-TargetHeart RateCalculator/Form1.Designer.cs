b74a497c00ab6baaab679b744c3d05533ac36be7
DamonWolf <43422067+DamonWolf@users.noreply.github.com>
2019-07-10 08:07:55 -0400

    Target Heartrate Calculator

---
+++ Damon Wolf-TargetHeart RateCalculator/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Damon_Wolf_TargetHeart_RateCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtBirthYr = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblMaxHeartRate = new System.Windows.Forms.Label();
+            this.lblTargetRange = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter your data then click Calculate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Your Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Your Birth Year";
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(205, 70);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(100, 22);
+            this.txtFName.TabIndex = 4;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(205, 107);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(100, 22);
+            this.txtLName.TabIndex = 5;
+            // 
+            // txtBirthYr
+            // 
+            this.txtBirthYr.Location = new System.Drawing.Point(205, 144);
+            this.txtBirthYr.Name = "txtBirthYr";
+            this.txtBirthYr.Size = new System.Drawing.Size(100, 22);
+            this.txtBirthYr.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(335, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "YYYY";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(16, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Heart Rate Calculations For";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(67, 291);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(181, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Your maxiumum Heart Rate";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(65, 335);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(202, 17);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Your target Heart Rate Range ";
+            // 
+            // lblMaxHeartRate
+            // 
+            this.lblMaxHeartRate.AutoSize = true;
+            this.lblMaxHeartRate.Location = new System.Drawing.Point(318, 291);
+            this.lblMaxHeartRate.Name = "lblMaxHeartRate";
+            this.lblMaxHeartRate.Size = new System.Drawing.Size(0, 17);
+            this.lblMaxHeartRate.TabIndex = 12;
+            // 
+            // lblTargetRange
+            // 
+            this.lblTargetRange.AutoSize = true;
+            this.lblTargetRange.Location = new System.Drawing.Point(318, 335);
+            this.lblTargetRange.Name = "lblTargetRange";
+            this.lblTargetRange.Size = new System.Drawing.Size(0, 17);
+            this.lblTargetRange.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(466, 291);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "BPM";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(469, 335);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 17);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "BPM";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(447, 64);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(89, 23);
+            this.btnCalc.TabIndex = 16;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(447, 104);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(89, 23);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(321, 222);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(0, 17);
+            this.lblUserName.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 401);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblTargetRange);
+            this.Controls.Add(this.lblMaxHeartRate);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBirthYr);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Target Heart Rate Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtBirthYr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblMaxHeartRate;
+        private System.Windows.Forms.Label lblTargetRange;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblUserName;
+    }
+}
+
---
+++ Damon Wolf-TargetHeart RateCalculator/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Damon_Wolf_TargetHeart_RateCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            string FirstName = txtFName.Text;
+            string LastName = txtLName.Text;
+            int BirthYear = Convert.ToInt32(txtBirthYr.Text);
+            lblUserName.Text = FirstName + " " + " " + LastName;
+
+            int Age = 2018 - BirthYear;
+            int MaxHR = 220 - Age;
+            double MinTargetRate = MaxHR * .50;
+            double MaxTargetRate = MaxHR * .85;
+
+            lblMaxHeartRate.Text = Convert.ToString(MaxHR);
+            lblTargetRange.Text = Convert.ToString(MinTargetRate + " " + " - " + " " + MaxTargetRate);
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+            txtFName.Text = "";
+            txtLName.Text = "";
+            txtBirthYr.Text = "";
+            lblMaxHeartRate.Text = "";
+            lblTargetRange.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
